c9fac80037558497dfbb0df844029e5b76df5d1e
harshitbanaula <harshitbanaula86@gmail.com>
2026-03-18 15:10:19 +0530

    remove pyparsing import line

--- backend/app/services/job_inference.py
+++ backend/app/services/job_inference.py
@@ -5,7 +5,6 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from pyparsing import line
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
